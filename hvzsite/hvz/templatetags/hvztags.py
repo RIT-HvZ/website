671f737ef745021147e5a41cef18497c93ed0a88
Chance Henderson <cman52894@gmail.com>
2023-09-18 15:44:10 -0400

    Hiding submit button for surveys from mods and admins

--- hvzsite/hvz/templatetags/hvztags.py
+++ hvzsite/hvz/templatetags/hvztags.py
@@ -8,6 +8,13 @@ def player_has_response(player, response):
     return PostGameSurveyResponse.objects.filter(player=player, response=response).count() > 0
 
 @register.simple_tag
+def can_respond_to_survey(player, survey):
+    status = player.current_status
+    return (survey.mission.team == 'h' and status.is_human()) or \
+           (survey.mission.team == "z" and status.is_zombie()) or \
+           status.is_admin()
+
+@register.simple_tag
 def get_player_response(player, survey):
     responses = PostGameSurveyResponse.objects.filter(player=player, survey=survey)
     if responses.count() > 0:
