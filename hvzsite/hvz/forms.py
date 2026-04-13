0cd24ad6259d81d00b92b48d6d703bec1ec6517a
Ava <transorsmth@gmail.com>
2026-04-13 09:25:57 -0400

    Possibly maybe hopefully fix post game survey

--- hvzsite/hvz/forms.py
+++ hvzsite/hvz/forms.py
@@ -233,7 +233,7 @@ class PostGameSurveyForm(forms.ModelForm):
                 if key.startswith("option_text_") and not key.startswith("option_text_id") or \
                       key.startswith("option_name_") and not key.startswith("option_name_id"):
                     self.fields[key] = forms.CharField()
-        if self.instance.id=='new':
+        if self.pk is None:
             return
         options = PostGameSurveyOption.objects.filter(
             survey=self.instance
